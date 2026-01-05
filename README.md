a850b3a80a577ad83853eb397f4fafe5275a7472
Marcus Tsang <marcuskmTsang@gmail.com>
2026-01-05 14:32:07 +0800

    started the project
    made task classes, added some basic pygame logic

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # goal_app
+
+projects, routines
+
+projects make more land, routines make buildings
+routine: add something or build onto something
+> when u do routines, you get some kind of income, which can be spent on stuff
+> terrain
+> monuments
+> buildings
+
+
+cubes - diagonal (polytopia style)
---
+++ main.py
@@ -0,0 +1,49 @@
+import pygame
+import random
+import math
+
+pygame.init()
+screen = pygame.display.set_mode((414, 896))
+
+class Task:
+    tasks = []
+    completedTasks = []
+    lastIndex = 0
+    def __init__(self, name):
+        self.name = name
+        self.index = Task.lastIndex
+        Task.tasks.append(self)
+        Task.lastIndex+=1
+
+    def complete(self):
+        Task.completedTasks.append(self)
+        Task.tasks.pop(self.index)
+    def delete(self): del self
+
+    @staticmethod
+    def printTasks():
+        print("---")
+        for t in Task.tasks: print(t.name)
+
+class Routine(Task):
+    def __init__(self, name, frequency):
+        super().__init__(name)
+
+class Project(Task):
+    def __init__(self, name, duration):
+        super().__init__(name)
+
+Task.printTasks()
+task = Task("task")
+task2 = Task("task2")
+task3= Task("task3")
+Task.printTasks()
+task2.complete()
+Task.printTasks()
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+pygame.quit()
